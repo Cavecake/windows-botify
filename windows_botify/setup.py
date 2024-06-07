1228862c796abbebbfc3db3e15224e3c3a4e6812
Cavecake <simon.maxton@web.de>
2024-06-07 21:09:32 +0200

    Setting up pip installation

---
+++ windows_botify/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='windows_botify',
+    version="0.0.1",
+    url='https://github.com/Cavecake/windows-botify',
+    author='cavecake',
+    author_email='cavecake@web.de',
+    description=('A simple libary to control windows'),
+    license='MIT',
+    packages=[
+    ],
+    install_requires = [
+
+    ]
+)
