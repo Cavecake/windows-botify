2dbb0d5f8460f3bf205599ae6a76ee18b02fc794
Cavecake <simon.maxton@web.de>
2024-06-20 18:41:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 from setuptools import setup, find_packages
-import subprocess
 
-version = "0.1.0"
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='windows_botify',
-    version=version,
+    version='0.1.0',
     url='https://github.com/Cavecake/windows-botify',
     author='cavecake',
     author_email='cavecake@web.de',
-    description=('A simple libary to control windows'),
+    description='A simple library to control windows',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
-    packages=['windows_botify'
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here if needed
     ],
-    install_requires = [
-
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    long_description_content_type='text/markdown'
 )
